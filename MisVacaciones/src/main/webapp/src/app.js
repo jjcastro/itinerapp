9172c8eff6b4c1a4885d59b23a61aa1d507da981
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-02-04 09:06:28 -0500

    Subir página de landing y cambiar estados a nested

--- MisVacaciones/src/main/webapp/src/app.js
+++ MisVacaciones/src/main/webapp/src/app.js
@@ -7,27 +7,35 @@
         }]);
 
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider.otherwise("/evento");
+            $urlRouterProvider.otherwise("/landing");
             $stateProvider
-                    .state('evento', {
+                    .state('dashboard.evento', {
                         url: '/evento',
                         templateUrl: "src/modules/evento/evento.tpl.html"
                     })
-                    .state('perfil',{
+                    .state('dashboard.perfil',{
                         url:'/perfil',
                         templateUrl:"src/modules/perfil/perfil.tpl.html"
                     })
-                    .state('mapa',{
+                    .state('dashboard.mapa',{
                                 url:'/mapa',
                         templateUrl:"src/modules/mapa/mapa.tpl.html"
                     })
-                    .state('login',{
+                    .state('dashboard.login',{
                                 url:'/login',
                         templateUrl:"src/modules/login/login.tpl.html"
                     })
-                    .state('registro',{
+                    .state('dashboard.registro',{
                                 url:'/registro',
                         templateUrl:"src/modules/registro/registro.tpl.html"
+                    })
+                    .state('landing',{
+                                url:'/landing',
+                        templateUrl:"src/modules/landing/landing.tpl.html"
+                    })
+                    .state('dashboard',{
+                                url:'/dashboard',
+                        templateUrl:"src/modules/dashboard/dashboard.tpl.html"
                     })
         }]);
 })(window.angular);
