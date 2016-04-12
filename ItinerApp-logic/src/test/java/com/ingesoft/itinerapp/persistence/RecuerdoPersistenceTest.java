07db2cd3766e38b33f0a81869f25f31c2033d8c6
Felipe <f.martinez11@uniandes.edu.co>
2016-04-12 10:56:29 -0500

    Dependencias a Arquillian en el POM
    Se agrego la dependencia a Arquillian en el POM

---
+++ ItinerApp-logic/src/test/java/com/ingesoft/itinerapp/persistence/RecuerdoPersistenceTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ingesoft.itinerapp.persistence;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.jboss.arquillian.junit.Arquillian;
+
+/**
+ *
+ * @author Lenovo
+ */
+@RunWith(Arquillian.class)
+public class RecuerdoPersistenceTest {
+    
+    public RecuerdoPersistenceTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
