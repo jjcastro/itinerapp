e256c5d9aeb1136d730f2c8252b24ac7d9817943
John Otalora <juanpocsc@hotmail.com>
2016-04-25 16:36:17 -0500

    Cambios A REST y demás

--- ItinerApp.pom/ItinerApp.web/src/test/java/com/ingesoft/itinerapp/resources/PerfilResourceTest.java
+++ ItinerApp.pom/ItinerApp.web/src/test/java/com/ingesoft/itinerapp/resources/PerfilResourceTest.java
@@ -5,35 +5,6 @@
  */
 package com.ingesoft.itinerapp.resources;
 
-import com.ingesoft.itinerapp.converter.RecuerdoConverter;
-import com.ingesoft.itinerapp.dtos.RecuerdoDTO;
-import java.io.File;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.junit.InSequence;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-import org.junit.Assert;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
-import uk.co.jemos.podam.api.PodamFactory;
-import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
  *
